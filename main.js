95b6dd8f857bd7e4f38e03b59166aae11b8ccc01
Janka K <janakorycanova@gmail.com>
2022-02-13 11:22:16 +0100

    nepovinny domaci ukol

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
+//document.querySelector("#vysledek").innerHTML = secti(4, 5);
+
+
 
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
@@ -13,9 +15,24 @@ function secti(a, b) {
   return c;
 }
 
+function klik(){
+  element = document.querySelector("#vysledek");
+  element.innerHTML = secti(4,5);
+}
+
+
 /**
  * Upozorní uživatele při spuštění.
  */
 function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
+  console.log("Gratulace, právě jsi spustila tuto funkci!");
+  element = document.querySelector('div.ctverecek');
+  element.style.lineHeight = '40px';
+  element.textContent = 'Gratulace,prave jsi spustila tuto funkci!';
+}
+
+function kliknuti(){
+  element = document.querySelector('.ctverecek');
+  element.style.backgroundColor = 'limegreen';
 }
